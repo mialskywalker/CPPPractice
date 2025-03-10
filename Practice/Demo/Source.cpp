88062dcbb24113181701985267c6a977ccff2b7c
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2025-03-10 16:16:26 +0100

    asd

--- Practice/Demo/Source.cpp
+++ Practice/Demo/Source.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+using namespace std;
+
+
+void welcome() {
+	cout << "Welcome!" << endl;
+}
+
+void printNumber(int numToPrint) {
+	cout << numToPrint << endl;
+}
+
+int add(int a, int b) {
+	int result;
+	result = a + b;
+	return result;
+}
+
 int main() {
-	std::cout << "You Died!" << std::endl;
+	
+	welcome();
+
+	printNumber(4);
+
+	int c;
+	c = add(1, 4);
+
+	printNumber(c);
 
 	system("pause");
 }
