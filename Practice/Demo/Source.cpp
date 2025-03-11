c9ae8b3db99dc02c6dd0943894412f445a84cd3a
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2025-03-11 14:18:07 +0100

    enums

--- Practice/Demo/Source.cpp
+++ Practice/Demo/Source.cpp
@@ -2,31 +2,45 @@
 
 using namespace std;
 
+enum PlayerStatus {
+	PS_Running,
+	PS_Walking,
+	PS_Crouching
+};
 
-void welcome() {
-	cout << "Welcome!" << endl;
-}
+const float RunSpeed = 800.f;
+const float WalkSpeed = 500.f;
+const float CrouchSpeed = 350.f;
 
-void printNumber(int numToPrint) {
-	cout << numToPrint << endl;
-}
-
-int add(int a, int b) {
-	int result;
-	result = a + b;
-	return result;
-}
+void UpdateMovementSpeed(PlayerStatus P_Status, float& speed);
 
 int main() {
 	
-	welcome();
+	float MovementSpeed;
 
-	printNumber(4);
+	PlayerStatus status = PS_Walking;
 
-	int c;
-	c = add(1, 4);
+	UpdateMovementSpeed(status, MovementSpeed);
 
-	printNumber(c);
+	cout << "Movement speed = " << MovementSpeed << endl;
+
 
 	system("pause");
+}
+
+void UpdateMovementSpeed(PlayerStatus P_Status, float& speed) {
+	switch (P_Status)
+	{
+	case PS_Running:
+		speed = RunSpeed;
+		break;
+	case PS_Walking:
+		speed = WalkSpeed;
+		break;
+	case PS_Crouching:
+		speed = CrouchSpeed;
+		break;
+	default:
+		break;
+	}
 }
