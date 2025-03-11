0d3851cd24bebe953221262293e9d1f0f9bc2d38
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2025-03-11 14:34:46 +0100

    structs

--- Practice/Demo/Source.cpp
+++ Practice/Demo/Source.cpp
@@ -2,45 +2,60 @@
 
 using namespace std;
 
-enum PlayerStatus {
-	PS_Running,
-	PS_Walking,
-	PS_Crouching
+struct LocationVector {
+	float X;
+	float Y;
+	float Z;
 };
 
-const float RunSpeed = 800.f;
-const float WalkSpeed = 500.f;
-const float CrouchSpeed = 350.f;
+struct Player {
+	int Level;
+	float Health;
+	float Damage;
+	float Stamina;
 
-void UpdateMovementSpeed(PlayerStatus P_Status, float& speed);
+	LocationVector Location = {0.f,0.f,0.f};
+
+	void TakeDamage(float dmg) {
+		Health -= dmg;
+	}
+
+	int GetLevel() {
+		if (Level > 10)
+		{
+			cout << "Level is greater than 10!" << endl;
+		}
+		return Level;
+	}
+
+	void DisplayLocation() {
+		cout << "Location.X = " << Location.X << endl;
+		cout << "Location.Y = " << Location.Y << endl;
+		cout << "Location.Z = " << Location.Z << endl;
+	}
+};
 
 int main() {
 	
-	float MovementSpeed;
+	Player p_1;
+	p_1.Level = 11;
+	p_1.Health = 100.f;
+	p_1.Damage = 10.f;
+	p_1.Stamina = 20.f;
 
-	PlayerStatus status = PS_Walking;
+	cout << "p_1 Level = " << p_1.GetLevel() << endl;
 
-	UpdateMovementSpeed(status, MovementSpeed);
+	p_1.TakeDamage(40.f);
 
-	cout << "Movement speed = " << MovementSpeed << endl;
+	cout << "p_1 takes " << 40.f << " damage" << endl;
+	cout << "p_1 Health = " << p_1.Health << endl;
 
+	p_1.DisplayLocation();
+
+	Player p_2 = { 1, 50.f, 40.f, 35.54f, {35.5f, 67.45f, 100.003f} };
+
+	p_2.DisplayLocation();
 
 	system("pause");
 }
 
-void UpdateMovementSpeed(PlayerStatus P_Status, float& speed) {
-	switch (P_Status)
-	{
-	case PS_Running:
-		speed = RunSpeed;
-		break;
-	case PS_Walking:
-		speed = WalkSpeed;
-		break;
-	case PS_Crouching:
-		speed = CrouchSpeed;
-		break;
-	default:
-		break;
-	}
-}
