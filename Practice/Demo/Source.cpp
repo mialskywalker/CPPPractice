c7e1780944e269318a985fbe69de2924c2ac09e6
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2025-03-14 18:36:31 +0100

    pointers

--- Practice/Demo/Source.cpp
+++ Practice/Demo/Source.cpp
@@ -2,59 +2,24 @@
 
 using namespace std;
 
-struct LocationVector {
-	float X;
-	float Y;
-	float Z;
-};
+struct Container {
+	string Name;
 
-struct Player {
-	int Level;
-	float Health;
-	float Damage;
-	float Stamina;
-
-	LocationVector Location = {0.f,0.f,0.f};
-
-	void TakeDamage(float dmg) {
-		Health -= dmg;
-	}
-
-	int GetLevel() {
-		if (Level > 10)
-		{
-			cout << "Level is greater than 10!" << endl;
-		}
-		return Level;
-	}
-
-	void DisplayLocation() {
-		cout << "Location.X = " << Location.X << endl;
-		cout << "Location.Y = " << Location.Y << endl;
-		cout << "Location.Z = " << Location.Z << endl;
-	}
+	int X;
+	int Y;
+	int Z;
 };
 
 int main() {
 	
-	Player p_1;
-	p_1.Level = 11;
-	p_1.Health = 100.f;
-	p_1.Damage = 10.f;
-	p_1.Stamina = 20.f;
+	
+	Container container = { "Sam", 5, 6, 7 };
 
-	cout << "p_1 Level = " << p_1.GetLevel() << endl;
+	Container* PtrToCont = &container;
 
-	p_1.TakeDamage(40.f);
 
-	cout << "p_1 takes " << 40.f << " damage" << endl;
-	cout << "p_1 Health = " << p_1.Health << endl;
-
-	p_1.DisplayLocation();
-
-	Player p_2 = { 1, 50.f, 40.f, 35.54f, {35.5f, 67.45f, 100.003f} };
-
-	p_2.DisplayLocation();
+	cout << (*PtrToCont).Name << endl;
+	cout << PtrToCont->Name << endl;
 
 	system("pause");
 }
