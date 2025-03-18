0ab88b50fd020508f555c5c731fd6aaeac657150
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2025-03-18 16:13:43 +0100

    polymorphism

--- Practice/Demo/Source.cpp
+++ Practice/Demo/Source.cpp
@@ -4,38 +4,45 @@ using namespace std;
 
 class Object {
 public:
-	virtual void BeginPlay() {
-		cout << "Object BeginPlay() called.\n";
-	}
+	virtual void BeginPlay();
 };
 
 class Actor : public Object {
 public:
-	virtual void BeginPlay() override {
-		cout << "Actor BeginPlay() called.\n";
-	}
+	virtual void BeginPlay() override;
 };
-
 
 class Pawn : public Actor {
 public:
-	virtual void BeginPlay() override {
-		cout << "Pawn BeginPlay() called.\n";
-	}
+	virtual void BeginPlay() override;
 };
 
 int main() {
-	Object* obj = new Object;
-	obj->BeginPlay();
+	
+	Object* ptr_to_object = new Object;
+	Actor* ptr_to_actor = new Actor;
+	Pawn* ptr_to_pawn = new Pawn;
 
-	Actor* act = new Actor;
-	act->BeginPlay();
+	Object* ObjectArray[] = { ptr_to_object, ptr_to_actor, ptr_to_pawn };
 
-	Pawn* pwn = new Pawn;
-	pwn->BeginPlay();
+	for (int i = 0; i < 3; i++)
+		ObjectArray[i]->BeginPlay();
 
-	delete obj;
-	delete act;
-	delete pwn;
+	delete ptr_to_object;
+	delete ptr_to_actor;
+	delete ptr_to_pawn;
 	system("pause");
 }
+
+void Object::BeginPlay() {
+	cout << "Object BeginPlay() called.\n\n";
+}
+
+void Actor::BeginPlay() {
+	cout << "Actor BeginPlay() called.\n\n";
+}
+
+void Pawn::BeginPlay() {
+	cout << "Pawn BeginPlay() called.\n\n";
+}
+
